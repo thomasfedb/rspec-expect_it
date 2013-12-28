ecc7dbec4977b12b1e3654e43bfb1c2947474222
Thomas Drake-Brockman <thom@sfedb.com>
2013-12-28 15:29:36 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in rspec-expect_it.gemspec
+gemspec
---
+++ lib/rspec/expect_it.rb
@@ -0,0 +1,7 @@
+require "rspec/expect_it/version"
+
+module Rspec
+  module ExpectIt
+    # Your code goes here...
+  end
+end
