85879edd0e4a63df7d491ac6a972819635516602
Thomas Drake-Brockman <thom@sfedb.com>
2015-07-17 21:25:42 +0800

    Added Appraisals for testing multiple Rubies.

---
+++ Appraisals
@@ -0,0 +1,5 @@
+["3.0", "3.1", "3.2", "3.3"].each do |rspec|
+  appraise "rspec-#{rspec}" do
+    gem "rspec", "~> #{rspec}"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
-require 'bundler/setup'
+require "bundler"
 
-require "rspec/expect_it"
+unless ENV["TRAVIS_CI"]
+  if RUBY_VERSION[0] == "2"
+    require "byebug"
+  else
+    require "debugger"
+  end
+end
+
+if ENV["TRAVIS_CI"]
+  Bundler.require
+else
+  Bundler.require(:default, :tools)
+end
 
 RSpec.configure do |config|
   config.order = 'random'
